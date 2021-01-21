33937419565db999533ed85a9ba597001e7183e1
rajaomariajaona <rajaomariajaona11@gmail.com>
2021-01-21 15:24:30 +0300

    add assets

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,58 +19,228 @@ function changeColors() {
 
 let cards = [
   {
-    title: "Title",
+    id: 1,
+    title: "Cliquez pour voir plus",
     type: ["web", "mobile"],
     image: "assets/images/hero.jpg",
     body: "Plateforme de Livraison <strong>Mtatitra </strong>",
     details: "",
+    captures: [
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "1",
+        },
+      },
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "2",
+        },
+      },
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "3",
+        },
+      },
+    ],
   },
   {
-    title: "Title",
+    id: 2,
+    title: "Cliquez pour voir plus",
     type: ["web", "mobile"],
     image: "assets/images/hero.jpg",
     body:
       "<strong>Markovid</strong> - Carte de representation des zones chaudes Covid à Madagascar",
     details: "",
+    captures: [
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "1",
+        },
+      },
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "2",
+        },
+      },
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "3",
+        },
+      },
+    ],
   },
   {
-    title: "Title",
+    id: 3,
+    title: "Cliquez pour voir plus",
     type: ["mobile"],
     image: "assets/images/hero.jpg",
     body: "Application de gestion du centre <strong> Berisheba</strong>",
     details: "",
+    captures: [
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "1",
+        },
+      },
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "2",
+        },
+      },
+      {
+        src: "assets/images/hero.jpg",
+        opts: {
+          caption: "3",
+        },
+      },
+    ],
   },
 
   {
-    title: "Title",
+    id: 4,
+    title: "Cliquez pour voir plus",
     type: ["web"],
-    image: "assets/images/hero.jpg",
+    image: "assets/images/agrizara/acceuil.gif",
     body:
       "<strong>AgriZara</strong> - Première place lors du <strong>FrontEnd Award 2019</strong> (organisé par Techzara)",
     details: "",
+    captures: [
+      {
+        src: "assets/images/agrizara/1.mp4",
+        opts: {
+          caption: "Acceuil",
+        },
+      },
+      {
+        src: "assets/images/agrizara/2.mp4",
+        opts: {
+          caption: "Agriculture malagasy",
+        },
+      },
+      {
+        src: "assets/images/agrizara/3.mp4",
+        opts: {
+          caption: "Offres et demande",
+        },
+      },
+      {
+        src: "assets/images/agrizara/4.mp4",
+        opts: {
+          caption: "Exchange box",
+        },
+      },
+    ],
   },
   {
-    title: "Title",
+    id: 5,
+    title: "Cliquez pour voir plus",
     type: ["web", "mobile"],
-    image: "assets/images/hero.jpg",
+    image: "assets/images/covid/acceuil.png",
     body:
       "<strong>Covid Tracker</strong> - Troisième place lors du <strong>Hackoragna 2020</strong>",
     details: "",
+    captures: [
+      {
+        src: "assets/images/covid/1.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/2.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/3.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/4.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/5.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/6.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/7.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/8.png",
+        opts: {
+          caption: "",
+        },
+      },
+      {
+        src: "assets/images/covid/9.png",
+        opts: {
+          caption: "",
+        },
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Cliquez pour voir plus",
+    type: ["desktop"],
+    image: "assets/images/rda/acceuil.jpg",
+    body:
+      "<strong>RDA</strong> - Remote Desktop App",
+    details: "",
+    captures: [
+      {
+        src: "assets/images/rda/1.mp4",
+        opts: {
+          caption: "Control",
+        },
+      },
+      {
+        src: "assets/images/rda/2.mp4",
+        opts: {
+          caption: "Transfer fichier",
+        },
+      },
+    ],
   },
 ];
 
-function generateCard(obj) {
+function generateCard(obj, index) {
   let type = "";
   for (const t of obj.type) {
     type += t + " ";
   }
-  return `<div class="col-md-3 col-10 my-3 grid-item ${type}" data-category="${type}">
+  return `<div class="col-md-3 col-10 my-3 grid-item ${type}" onclick="javascript:openFancy(${index});" data-src="#owl${obj.id}" data-category="${type}">
     <div class="card">
         <div class="container-card-img">
             <img class="card-img" src="${obj.image}" alt="">
             <div class="container-card-img-button mx-0 row justify-content-center">
                 <div>
-                   <h3>${obj.title}</h3>
+                   <center> <h5>${obj.title}</h5> </center>
                 </div>
             </div>
         </div>
@@ -82,7 +252,11 @@ function generateCard(obj) {
 }
 
 $(document).ready(function () {
-  changeColors();
+    var typed = new Typed('.test', {
+        stringsElement: '.text-home',
+        typeSpeed: 30
+    });
+    changeColors();
   $(window).scroll(changeColors);
   $(".more-info").click((e) => {
     let currentButton = $(e.currentTarget);
@@ -98,10 +272,25 @@ $(document).ready(function () {
     }
   });
   let cardsDom = "";
-  for (let card of cards) {
-    cardsDom += generateCard(card);
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    cardsDom += generateCard(card, i);
   }
   $("#cards").html(cardsDom);
+  //   $(".owl-carousel").owlCarousel();
+  $("[data-fancybox]").fancybox({
+    buttons: [
+      "zoom",
+      //"share",
+      "slideShow",
+      "fullScreen",
+      "download",
+      "thumbs",
+      "close",
+    ],
+    protect: true,
+    preventCaptionOverlap: true,
+  });
 });
 
 function filter(event, type) {
@@ -115,4 +304,18 @@ function filter(event, type) {
   $(event.currentTarget).addClass("active");
   console.log(type);
   $grid.isotope({ filter: type });
+}
+function openFancy(index) {
+  $.fancybox.open(cards[index].captures, {
+    buttons: [
+      "zoom",
+      "slideShow",
+      "fullScreen",
+      "thumbs",
+      "close",
+    ],
+    protect: true,
+    preventCaptionOverlap: true,
+    loop: true,
+  });
 }
